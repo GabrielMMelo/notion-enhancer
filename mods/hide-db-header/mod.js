10e91308ca9bac87743bca44dc0fa14a6e8dc3a5
Gabriel Melo <gabrielmelocomp@gmail.com>
2020-10-20 13:22:44 -0300

    Add new extension: hide-db-header

---
+++ mods/hide-db-header/mod.js
@@ -0,0 +1,18 @@
+/*
+ * focus mode
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 GabrielMMelo <gabrielmelocomp@gmail.com> (https://gabrielmmelo.github.io/)
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: '808b3b9b-43b1-4fa8-bc3f-3272762c81fe',
+  tags: ['extension'],
+  name: 'hide db header',
+  desc:
+    'hide the header of a linked database(will be shown on hover).',
+  version: '0.1.0',
+  author: 'gabrielmmelo',
+};
